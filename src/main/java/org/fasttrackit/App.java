3f1d02827671d4bedac2f5dd79faf56cbf10e031
Alex Schiotis <35920550+AlexSchiotis@users.noreply.github.com>
2018-05-30 10:21:07 +0300

    initial commit

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,6 +10,10 @@ public class App
     // todo:Don't forget to ....
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        System.out.println( "Hello New World!" );
+        System.out.println( "Hello New World!" );
+
     }
 }
